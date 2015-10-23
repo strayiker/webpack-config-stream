15326421a2f510b23a0f77f25214212aae56c9ad
Marat Dreizin <marat.dreizin@gmail.com>
2015-10-23 21:38:59 +0300

    test(compilerAdapter): adds tests

---
+++ test/compilerAdapter.spec.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var expect = require('expect.js'),
+    fs = require('fs'),
+    path = require('path'),
+    gutil = require('gulp-util'),
+    CompilerAdapter = require('../lib/compilerAdapter');
+
+var FILENAME = path.resolve('test/fixtures/compilerAdapter/webpack.config.js');
+
+describe('CompilerAdapter', function () {
+    var compilerAdapter,
+        file;
+
+    beforeEach(function() {
+        compilerAdapter = new CompilerAdapter({
+            useMemoryFs: true,
+            progress: false
+        });
+
+        file = new gutil.File({
+            base: path.basename(FILENAME),
+            path: FILENAME,
+            contents: fs.readFileSync(FILENAME)
+        });
+    });
+
+    context('#run()', function() {
+        it('should compile config successfully', function(done) {
+            compilerAdapter.run(file, function(err, stats) {
+                expect(err).to.be(null);
+                expect(stats).to.be.an(Object);
+
+                done();
+            });
+        });
+
+        it('should throw error when "callback" is not "Function"', function() {
+            expect(compilerAdapter.run).withArgs(file).to.throwError();
+        });
+    });
+
+    context('#watch()', function() {
+        it('should compile config successfully', function(done) {
+            compilerAdapter.watch(file, done);
+        });
+
+        it('should recompile config successfully', function(done) {
+            compilerAdapter.watch(file, function() {
+                compilerAdapter.watch(file, function(err, stats) {
+                    expect(err).to.be(null);
+                    expect(stats).to.be.an(Object);
+
+                    done();
+                });
+            });
+        });
+
+        it('should throw error when "callback" is not "Function"', function() {
+            expect(compilerAdapter.watch).withArgs(file).to.throwError();
+        });
+    });
+});
---
+++ test/fixtures/compilerAdapter/webpack.config.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var WebpackConfig = require('webpack-config');
+
+module.exports = new WebpackConfig().merge({});
