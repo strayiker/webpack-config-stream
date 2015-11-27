426efee4812efd66a48d1b3f342f6b4158d7d385
Marat Dreizin <marat.dreizin@gmail.com>
2015-11-27 14:21:13 +0300

    test(config): replaces `[]` with `{}`

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,23 @@
 'use strict';
 
 var WebpackConfig = require('webpack-config'),
+    _ = require('lodash'),
     Index = require('../index');
 
 describe('index', function () {
     it('should have static properties', function() {
-        ['run', 'format', 'failAfter', 'closest', 'watch', 'proxy', 'props', 'init', 'webpack'].forEach(function(name) {
-            expect(Index[name]).toEqual(jasmine.any(Function));
+        _.each({
+            run: Function,
+            format: Function,
+            failAfter: Function,
+            closest: Function,
+            watch: Function,
+            proxy: Function,
+            props: Function,
+            init: Function,
+            webpack: Function
+        }, function(value, key) {
+            expect(_.get(Index, key)).toEqual(jasmine.any(value));
         });
 
         expect(Index.Config).toEqual(WebpackConfig);
